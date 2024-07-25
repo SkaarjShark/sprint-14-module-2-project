8d48c5f127a920c7978e835ad2c4b14fe67c74c6
Jacob Handley <skaarjshark@outlook.com>
2024-07-25 16:43:52 -0500

    clean butt

--- api/server.js
+++ api/server.js
@@ -9,4 +9,14 @@ server.disable('x-powered-by')
 
 server.use('/api/cars', carsRouter)
 
+server.use('*', (req, res, next) => {
+    next({ status: 404, message: 'not found'})
+})
+
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
+
 module.exports = server
